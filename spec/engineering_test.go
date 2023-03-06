176223cd803c5afd92777b7d05fa44cdfcb61887
Michael Jeuga <mikejeuga@gmail.com>
2023-03-06 16:39:51 +0000

    ok

--- spec/engineering_test.go
+++ spec/engineering_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/adamluzsi/testcase"
 	"github.com/mikejeuga/dubs/team"
-	"math/rand"
 	"testing"
 )
 
@@ -20,7 +19,39 @@ type StubTeam struct {
 }
 
 func (s *StubTeam) Find(ctx context.Context, engineerID int) (team.Engineer, error) {
-	return s.Members[engineerID], nil
+	for _, engineer := range s.Members {
+		if engineer.ID == engineerID {
+			return engineer, nil
+		}
+	}
+	return team.Engineer{}, nil
+}
+
+type SpyTeam struct {
+	Members    team.Squad
+	findCalled bool
+	findCalls  int
+}
+
+func (sp *SpyTeam) Find(ctx context.Context, engineerID int) (team.Engineer, error) {
+	for _, engineer := range sp.Members {
+		if engineer.ID == engineerID {
+			sp.findCalledTrue()
+			sp.Called()
+			return engineer, nil
+		}
+	}
+	return team.Engineer{}, nil
+}
+
+func (sp *SpyTeam) findCalledTrue() bool {
+	sp.findCalled = true
+	return sp.findCalled
+}
+
+func (sp *SpyTeam) Called() int {
+	sp.findCalls += 1
+	return sp.findCalls
 }
 
 func TestFind(t *testing.T) {
@@ -38,8 +69,7 @@ func TestFind(t *testing.T) {
 	})
 
 	aTeam := testcase.Let(s, func(t *testcase.T) *team.Team {
-		teamMembers := make(map[int]team.Engineer)
-
+		teamMembers := make(team.Squad, 0)
 		theTeam := team.Team{Members: teamMembers}
 		return &theTeam
 	})
@@ -47,6 +77,7 @@ func TestFind(t *testing.T) {
 	SaltPay := testcase.Let(s, func(t *testcase.T) *team.Company {
 		teams := make([]team.Team, 0)
 		squadron := *aTeam.Get(t)
+		squadron.Members = append(squadron.Members, *anEngineer.Get(t))
 		teams = append(teams, squadron)
 		return team.NewCompany(teams)
 	})
@@ -57,35 +88,51 @@ func TestFind(t *testing.T) {
 
 	StubbyTeam := testcase.Let(s, func(t *testcase.T) *StubTeam {
 		squad := make(team.Squad, 0)
-		squad[anEngineer.Get(t).ID] = *anEngineer.Get(t)
+		squad = append(squad, *anEngineer.Get(t))
 		return &StubTeam{Members: squad}
 	})
 
+	theSpyTeam := testcase.Let(s, func(t *testcase.T) *SpyTeam {
+		squad := make(team.Squad, 0)
+		squad = append(squad, *anEngineer.Get(t))
+		return &SpyTeam{
+			Members: squad,
+		}
+	})
+
 	s.Describe("Dummy", func(s *testcase.Spec) {
+		act := func(t *testcase.T) (team.Engineer, error) {
+			return SaltPay.Get(t).FindEngineer(ctx, dummyTeam.Get(t))
+		}
 		s.Test("When the organisation is looking for an Engineer within a Dummy Team", func(t *testcase.T) {
 			//The dependency I inject here is irrelevant, it is almost noop. Here the dummy team does nothing.
-			act := func(t *testcase.T) (team.Engineer, error) {
-				return SaltPay.Get(t).FindEngineer(ctx, dummyTeam.Get(t), rand.Int())
-			}
-
-			s.Then("It find the engineer", func(t *testcase.T) {
-				_, err := act(t)
-				t.Must.NoError(err)
-			})
+			_, err := act(t)
+			t.Must.NoError(err)
 		})
 	})
 
 	s.Describe("Stub", func(s *testcase.Spec) {
+		act2 := func(t *testcase.T) (team.Engineer, error) {
+			return SaltPay.Get(t).FindEngineer(ctx, StubbyTeam.Get(t))
+		}
 		s.Test("When the organisation is looking for an Engineer within a Stubby Team", func(t *testcase.T) {
-			act2 := func(t *testcase.T) (team.Engineer, error) {
-				return SaltPay.Get(t).FindEngineer(ctx, StubbyTeam.Get(t), anEngineer.Get(t).ID)
-			}
+			engineer, err := act2(t)
+			t.Must.NoError(err)
+			t.Must.NotNil(engineer)
+			t.Must.Equal("Salter", engineer.LastName)
+		})
+	})
 
-			s.Then("It find the engineer", func(t *testcase.T) {
-				engineer, err := act2(t)
-				t.Must.NoError(err)
-				t.Must.Equal("Salter", engineer.LastName)
-			})
+	s.Describe("Spy", func(s *testcase.Spec) {
+		act2 := func(t *testcase.T) (team.Engineer, error) {
+			return SaltPay.Get(t).FindEngineer(ctx, theSpyTeam.Get(t))
+		}
+		s.Test("When the organisation is looking for an Engineer within a Spy Team", func(t *testcase.T) {
+			engineer, err := act2(t)
+			t.Must.NoError(err)
+			t.Must.NotNil(engineer)
+			t.Must.True(theSpyTeam.Get(t).findCalledTrue())
+			t.Must.Equal(1, theSpyTeam.Get(t).findCalls)
 		})
 	})
 
--- team/organistion.go
+++ team/organistion.go
@@ -17,12 +17,27 @@ func NewCompany(engineering []Team) *Company {
 	return &Company{Engineering: engineering}
 }
 
-type Squad map[int]Engineer
+type Squad []Engineer
 
-func (c *Company) FindEngineer(ctx context.Context, team EngineerFinder, id int) (Engineer, error) {
+func (c *Company) FindEngineer(ctx context.Context, team EngineerFinder) (Engineer, error) {
+	//engineer, err := team.Find(ctx, id)
+	//if err != nil {
+	//	return Engineer{}, err
+	//}
+	//return engineer, nil
+	var id int
+
+	for _, engTeam := range c.Engineering {
+		if len(engTeam.Members) > 0 {
+			id = engTeam.Members[0].ID
+		}
+		break
+	}
+
 	engineer, err := team.Find(ctx, id)
 	if err != nil {
 		return Engineer{}, err
 	}
-	return engineer, nil
+
+	return engineer, err
 }
